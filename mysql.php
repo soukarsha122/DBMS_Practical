4d906aeba181df4a4e2d8d365a6fb8f134ae2633
soukarsha122 <78530146+soukarsha122@users.noreply.github.com>
2022-12-23 00:13:05 +0530

    Delete mysql.php

--- mysql.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Connect to the database
-$conn = new mysqli("localhost", "root", "", "art_gallery");
-
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
